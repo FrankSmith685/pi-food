c526d85ed320264db8e2098b689f3de204c6234a
12frankporx <f.s.b.rojas@gmail.com>
2022-10-20 17:32:15 -0500

    se modifico

--- client/src/index.js
+++ client/src/index.js
@@ -14,16 +14,16 @@ dotenv.config();
 axios.defaults.baseURL = "https://app-pi-demo-food.herokuapp.com";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-ReactDOM.render(
+root.render(
   <Provider store={store}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
