e7d58b840b41824e63f27139e0304f89320ceb85
12frankporx <f.s.b.rojas@gmail.com>
2022-10-20 17:27:54 -0500

    se modifico

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import store from './redux/store/index.js';
+import axios from 'axios'
 import dotenv from "dotenv";
+
 
 dotenv.config();
 axios.defaults.baseURL = "https://app-pi-demo-food.herokuapp.com";
